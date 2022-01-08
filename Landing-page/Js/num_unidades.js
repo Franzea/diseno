c9791bfc62b2d856dffb3e18ff30b161decfbff6
franzea <fzg0001@alu.medac.es>
2022-01-08 21:31:52 +0100

    7-1-2022

--- Landing-page/Js/num_unidades.js
+++ Landing-page/Js/num_unidades.js
@@ -1,5 +1,5 @@
 //definimos un numero aleatorio entre 100 y 8 para declarar un número de unidades
-let numunidades = parseInt(Math.random() * (100 - 8) + 8); 
+let numunidades = parseInt(Math.random() * (40 - 8) + 8); 
 
 //Cambiamos el número 
 document.querySelector("span").textContent = numunidades;
